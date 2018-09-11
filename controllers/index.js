025d1e505a432c3f282a6174670a1399fa8a6aa1
Xuroth <xuroth@gmail.com>
2018-09-10 17:09:50 -0700

    Updated gitignore, added view files/placeholders

--- controllers/index.js
+++ controllers/index.js
@@ -14,4 +14,22 @@ module.exports = (app, ...rest) => {
             locals.settings.title = 'B2D | Home';
             res.render('pages/base/home', locals);
         })
+    
+    app.route('/about')
+        .get( (req, res) => {
+            let locals = {
+                settings: viewSettings
+            }
+            locals.settings.title = 'B2D | About Us';
+            res.render('pages/base/about', locals)
+        })
+
+    app.route('/contact')
+        .get( (req, res) => {
+            let locals = {
+                settings: viewSettings
+            }
+            locals.settings.title = 'B2D | Contact Us';
+            res.render('pages/base/contact', locals)
+        })
 }
